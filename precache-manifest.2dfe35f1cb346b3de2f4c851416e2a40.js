278964a814ded35d970fbf1652611dfe2571e492
Mary <maryliu.388@gmail.com>
2020-04-13 12:58:41 -0400

    Updates

---
+++ precache-manifest.2dfe35f1cb346b3de2f4c851416e2a40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df225753a0922434a3d0f11b06508dc3",
+    "url": "/zodiac/index.html"
+  },
+  {
+    "revision": "136bf8d4a1248b1bd066",
+    "url": "/zodiac/static/css/main.b4c81b25.chunk.css"
+  },
+  {
+    "revision": "c6676f0df40bc5b1fc5d",
+    "url": "/zodiac/static/js/2.452cf433.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zodiac/static/js/2.452cf433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136bf8d4a1248b1bd066",
+    "url": "/zodiac/static/js/main.999cacfc.chunk.js"
+  },
+  {
+    "revision": "5467df223fb0d294399d",
+    "url": "/zodiac/static/js/runtime-main.eaba4905.js"
+  }
+]);
